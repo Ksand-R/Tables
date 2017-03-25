91c25bc7db161d8d0e46a59443d3012c14146075
Alexander Romanov <al.romanov2012@yandex.ru>
2017-03-26 02:02:20 +0300

    Added a gtest progect. Added a project with tests

--- main_proj/main.cpp
+++ main_proj/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include "Monom.h"
 #include "Polynomial.h"
-#include "Abstract_tab.h"
+#include "Tabl_proj.h"
 using namespace std;
 
 int main() {
+
+
 	//Polynomial P1;
 	//Polynomial P2;
 	//P1.Input_poly();
 	//P2 = P1;
 	//Polynomial P3 = P1+P2;
 	//P3.Print_poly();
+	
 
 	cout << "I'm alive yet" << endl;
+
 	return 0;
 }
---
+++ tests/test_main.cpp
@@ -0,0 +1,28 @@
+#include <gtest.h>
+#include "Polynomial.h"
+//#include "../Tables/Table.h"
+int main(int argc, char **argv) {::testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+//	Polynomial p1, p2, p3;
+//	//p1.add_monom_in_tail(new Monom(2.3, 2));
+//	//p2.add_monom_in_tail(new Monom(2.1, 3));
+//	//p2.add_monom_in_tail(new Monom(-2.1, 3));	
+//	/*p1.Input_poly();
+//	p2.Input_poly();*/
+//	//NodeT a;
+//	//a.setname("qqq");
+//	//a.ptr_p = &p1;
+//	
+//	
+//	/*p3 = p1 * p2;
+//	p3.Print_poly();*/
+//
+//	//a.setname("asd");
+//
+//	/*cout << a.name << endl;
+//	cout << a.ptr_p << endl;
+//	*///
+//	return RUN_ALL_TESTS();
+//
+return 0;
+}
