0ab98db68aa382ce1088b500e81c91715fc60092
Alexander Romanov <al.romanov2012@yandex.ru>
2017-03-26 01:24:47 +0300

    added a modyfied Polynomials lab

---
+++ Polynomials/Polynomial.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <cstdio>
+#include <limits>
+#include "Monom.h"
+using namespace std;
+
+class Polynomial {
+private:
+	Monom* head;
+	Monom* tail;
+	int size; // counter of amount of monoms
+
+public:
+	Polynomial();
+	Polynomial(const Polynomial& p);
+	void Input_poly();
+	void bubble_sort();
+	void swap_(Monom* m1, Monom* m2);
+	void add_monom_in_tail(Monom* m);
+	void del_monom(Monom *ptr2);
+	const Polynomial& operator = (const Polynomial &p);
+	Polynomial operator + (Polynomial p);
+	Polynomial operator - (Polynomial m);
+	Polynomial operator * (double d);
+	Polynomial operator * (Monom m);
+	Polynomial operator * (Polynomial m);
+	void Print_poly();
+	void bring_similar();
+	bool operator==(const Polynomial& rhs) const;
+	~Polynomial();
+};
---
+++ main_proj/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Monom.h"
+#include "Polynomial.h"
+#include "Abstract_tab.h"
+using namespace std;
+
+int main() {
+	//Polynomial P1;
+	//Polynomial P2;
+	//P1.Input_poly();
+	//P2 = P1;
+	//Polynomial P3 = P1+P2;
+	//P3.Print_poly();
+
+	cout << "I'm alive yet" << endl;
+	return 0;
+}
