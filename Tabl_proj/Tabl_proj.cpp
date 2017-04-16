29d8674c3240fda6fd399079c7b19035392836e0
Alexander Romanov <al.romanov2012@yandex.ru>
2017-04-16 10:41:39 +0300

    proj_files

--- Tabl_proj/Tabl_proj.cpp
+++ Tabl_proj/Tabl_proj.cpp
@@ -2,28 +2,36 @@
 
 Row::Row(string n, Polynomial* p) {
 	name = n;
-	ptr_p = new Polynomial(*p);
+	ptr_p = new Polynomial(*p); 
+	is__empty = false;
 }
 Row& Row::operator =(const Row& n) {
 	if (this == &n) { return *this; }
 	else {
 		name = n.name;
 		ptr_p = new Polynomial(*n.ptr_p);
+		is__empty = n.is__empty;
 		return *this;
 	}
 }
+Row::Row(const Row& r)
+{
+	name = r.name;
+	if (r.ptr_p != NULL)
+		ptr_p = new Polynomial(*r.ptr_p);
+	else ptr_p = NULL;
+	is__empty = r.is__empty;
+}
 
+Row::~Row()
+{
+	if (ptr_p) {
+		name = "emp";
+		delete ptr_p;
+	}
+}
 ostream& operator << (ostream& os, const Row& row)
 {
 	os << row.name << "		"<< *(row.ptr_p);
 	return os;
 }
-
-//
-//Table::Table(){
-//	Row* array[SIZE] = {};
-//	for (int i(0); i < SIZE; ++i) {
-//		(*array[i]).name = "None";
-//	}
-//	count_rows = 0;
-//}
